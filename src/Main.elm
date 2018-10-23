4edf5e58055feb71c4a287343a47f94f23a87cf2
evelynting610 <esting610@gmail.com>
2018-10-23 16:04:54 -0500

    unstyled table

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,8 @@
-module AudienceList exposing (..)
+module Main exposing (..)
+import Browser
+import Bootstrap.Table as Table
 
-import Html exposing (text)
+import Html exposing (..)
 
 main =
   Browser.sandbox { init = init, update = update, view = view }
@@ -11,27 +13,68 @@ type alias Audience =
     name : String,
     description : String,
     creator : String,
-    date_updated : Date
+    date_updated : String
   }
 
 type alias Model =
-  { audiences : List Audience
+  { audiences : List Audience,
+    error : String
+  }
+
+init : Model
+init =
+  { audiences = [ 
+    {
+      id = 1,
+      name = "Elderly Rock Listeners",
+      description = "",
+      creator = "Evelyn",
+      date_updated = "October 3, 2018"
+    },
+    {
+      id = 2,
+      name = "Young Wine Drinkers",
+      description = "",
+      creator = "Evelyn",
+      date_updated = "October 7, 2018"
+    }
+  ],
+  error = ""
   }
 
 
 -- UPDATE
 
--- type Msg = Reset | ...
+type Msg = Reset
 
--- update : Msg -> Model -> Model
--- update msg model =
---   case msg of
---     Reset -> ...
---     ...
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Reset -> { model | error = ""  }
 
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-  ...
+    Table.table
+      { options = [ Table.striped ]
+      , thead = Table.simpleThead
+            (List.map formattedHeader ["Name", "Creator", "Date Updated"])
+      , tbody = Table.tbody
+            []
+            (List.map formattedRow model.audiences)
+      }
+
+formattedHeader : String  -> Table.Cell msg
+formattedHeader message =
+  Table.th [] [ text message ]
+
+formattedRow : Audience  -> Table.Row msg
+formattedRow audience =
+  Table.tr 
+    []
+    [ Table.td [] [ text audience.name ]
+    , Table.td [] [ text audience.creator ]
+    , Table.td [] [ text audience.date_updated ]
+    ]
